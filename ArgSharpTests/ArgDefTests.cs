c8c4e911ae758034d93f906c7bdce6ba46e17a05
codyThompson <cody.thompson22@gmail.com>
2015-10-07 21:38:35 -0700

    Added a unit test project.

---
+++ ArgSharpTests/ArgDefTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using ArgSharp;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ArgSharpTests
+{
+    [TestClass]
+    public class ArgDefTests
+    {
+        [TestMethod]
+        public void parseInitTestLabelName()
+        {
+            ArgDef testDef = new ArgDef();
+            testDef.argLabels.AddRange(new string[] {"-t", "-test1", "-test22", "-test0"});
+            testDef.parseInit(ArgTypeParser.basicParsers);
+
+            Assert.AreEqual<string>("test22", testDef.name);
+        }
+
+        [TestMethod]
+        public void parseInitTestType()
+        {
+            ArgDef testDef = new ArgDef();
+            testDef.argLabels.AddRange(new string[] { "-t", "-test1", "-test22", "-test0" });
+            testDef.parseInit(ArgTypeParser.basicParsers);
+
+            Assert.AreEqual<Type>(typeof(bool), testDef.type);
+
+            testDef = new ArgDef();
+            testDef.argLabels.AddRange(new string[] { "-t", "-test1", "-test22", "-test0" });
+            testDef.argCount++;
+            testDef.parseInit(ArgTypeParser.basicParsers);
+
+            Assert.AreEqual<Type>(typeof(string), testDef.type);
+
+            testDef = new ArgDef();
+            testDef.argLabels.AddRange(new string[] { "-t", "-test1", "-test22", "-test0" });
+            testDef.argCount++;
+            testDef.type = typeof(double);
+            testDef.parseInit(ArgTypeParser.basicParsers);
+
+            Assert.AreEqual(typeof(double), testDef.type);
+        }
+    }
+}
