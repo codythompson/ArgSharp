5c79bc21db6b7064f1484ea0232ff698d9c66fb6
codyThompson <cody.thompson22@gmail.com>
2015-10-08 18:56:44 -0700

    Added some unit tests for the ArgDef.parseInit method. These tests check that the required flag gets set correctly.

--- ArgSharpTests/ArgDefTests.cs
+++ ArgSharpTests/ArgDefTests.cs
@@ -7,6 +7,9 @@ namespace ArgSharpTests
     [TestClass]
     public class ArgDefTests
     {
+        /*
+         * The following test the parseInit method
+         */
         [TestMethod]
         public void parseInitTestLabelName()
         {
@@ -14,7 +17,7 @@ namespace ArgSharpTests
             testDef.argLabels.AddRange(new string[] {"-t", "-test1", "-test22", "-test0"});
             testDef.parseInit(ArgTypeParser.basicParsers);
 
-            Assert.AreEqual<string>("test22", testDef.name, "Wrong name chose from labels.");
+            Assert.AreEqual<string>("test22", testDef.name, "[ArgDef][parseInit] Wrong name chose from labels.");
         }
 
         [TestMethod]
@@ -24,14 +27,14 @@ namespace ArgSharpTests
             testDef.argLabels.AddRange(new string[] { "-t", "-test1", "-test22", "-test0" });
             testDef.parseInit(ArgTypeParser.basicParsers);
 
-            Assert.AreEqual<Type>(typeof(bool), testDef.type, "Expecting type bool for a labeled arg of argCount 0.");
+            Assert.AreEqual<Type>(typeof(bool), testDef.type, "[ArgDef][parseInit] Expecting type bool for a labeled arg of argCount 0.");
 
             testDef = new ArgDef();
             testDef.argLabels.AddRange(new string[] { "-t", "-test1", "-test22", "-test0" });
             testDef.argCount++;
             testDef.parseInit(ArgTypeParser.basicParsers);
 
-            Assert.AreEqual<Type>(typeof(string), testDef.type, "Expecting type string as the type for a labeled arg of argCount > 0 with no specified type.");
+            Assert.AreEqual<Type>(typeof(string), testDef.type, "[ArgDef][parseInit] Expecting type string as the type for a labeled arg of argCount > 0 with no specified type.");
 
             testDef = new ArgDef();
             testDef.argLabels.AddRange(new string[] { "-t", "-test1", "-test22", "-test0" });
@@ -39,7 +42,29 @@ namespace ArgSharpTests
             testDef.type = typeof(double);
             testDef.parseInit(ArgTypeParser.basicParsers);
 
-            Assert.AreEqual(typeof(double), testDef.type, "Type was set to double, but the type after parseInit was not double.");
+            Assert.AreEqual(typeof(double), testDef.type, "[ArgDef][parseInit] Type was set to double, but the type after parseInit was not double.");
+        }
+
+        [TestMethod]
+        public void parseInitTestRequired()
+        {
+            ArgDef testDef = new ArgDef();
+            testDef.name = "atest";
+            testDef.parseInit(ArgTypeParser.basicParsers);
+
+            Assert.AreEqual<bool>(true, testDef.required, "[ArgDef][parseInit] Ordered args should be required.");
+
+            testDef = new ArgDef();
+            testDef.argLabels.Add("-t");
+            testDef.parseInit(ArgTypeParser.basicParsers);
+
+            Assert.AreEqual<bool>(false, testDef.required, "[ArgDef][parseInit] Labeled args should not be required unless required was explicitly set.");
+
+            testDef = new ArgDef();
+            testDef.argLabels.Add("-t");
+            testDef.required = true;
+
+            Assert.AreEqual<bool>(true, testDef.required, "[ArgDef][parseInit] Labeled args should be required when required is explicitly set.");
         }
     }
 }
