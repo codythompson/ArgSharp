b1df7c03fdfbf6b62c95ac0fbe51b53cc69c3fad
codyThompson <cody.thompson22@gmail.com>
2015-10-08 18:40:20 -0700

    Added failure messages to existing unit tests.

--- ArgSharpTests/ArgDefTests.cs
+++ ArgSharpTests/ArgDefTests.cs
@@ -14,7 +14,7 @@ namespace ArgSharpTests
             testDef.argLabels.AddRange(new string[] {"-t", "-test1", "-test22", "-test0"});
             testDef.parseInit(ArgTypeParser.basicParsers);
 
-            Assert.AreEqual<string>("test22", testDef.name);
+            Assert.AreEqual<string>("test22", testDef.name, "Wrong name chose from labels.");
         }
 
         [TestMethod]
@@ -24,14 +24,14 @@ namespace ArgSharpTests
             testDef.argLabels.AddRange(new string[] { "-t", "-test1", "-test22", "-test0" });
             testDef.parseInit(ArgTypeParser.basicParsers);
 
-            Assert.AreEqual<Type>(typeof(bool), testDef.type);
+            Assert.AreEqual<Type>(typeof(bool), testDef.type, "Expecting type bool for a labeled arg of argCount 0.");
 
             testDef = new ArgDef();
             testDef.argLabels.AddRange(new string[] { "-t", "-test1", "-test22", "-test0" });
             testDef.argCount++;
             testDef.parseInit(ArgTypeParser.basicParsers);
 
-            Assert.AreEqual<Type>(typeof(string), testDef.type);
+            Assert.AreEqual<Type>(typeof(string), testDef.type, "Expecting type string as the type for a labeled arg of argCount > 0 with no specified type.");
 
             testDef = new ArgDef();
             testDef.argLabels.AddRange(new string[] { "-t", "-test1", "-test22", "-test0" });
@@ -39,7 +39,7 @@ namespace ArgSharpTests
             testDef.type = typeof(double);
             testDef.parseInit(ArgTypeParser.basicParsers);
 
-            Assert.AreEqual(typeof(double), testDef.type);
+            Assert.AreEqual(typeof(double), testDef.type, "Type was set to double, but the type after parseInit was not double.");
         }
     }
 }
