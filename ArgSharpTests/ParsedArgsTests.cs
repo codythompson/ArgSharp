55cf89ca2b0e441fe5da301ad6c55f6094374c83
codyThompson <cody.thompson22@gmail.com>
2015-11-08 10:18:21 -0800

    Added tests for errorOccured and containsKey for ParsedArgs.

--- ArgSharpTests/ParsedArgsTests.cs
+++ ArgSharpTests/ParsedArgsTests.cs
@@ -26,5 +26,25 @@ namespace ArgSharpTests
             Assert.AreEqual<string>("Test Error Message 1", args.getErrorMessages()[0], "[ParsedArgs][addErrorMessages] Unexpected error message encountered.");
             Assert.AreEqual<string>("Test Error Message 2", args.getErrorMessages()[1], "[ParsedArgs][addErrorMessages] Unexpected error message encountered.");
         }
+
+        [TestMethod]
+        [TestCategory("ParsedArgs")]
+        public void errorOccuredTest()
+        {
+            ParsedArgs args = new ParsedArgs();
+            Assert.IsFalse(args.errorOccured(), "[ParsedArgs][errorOccured] Expected false.");
+            args.addErrorMessage("Test message");
+            Assert.IsTrue(args.errorOccured(), "[ParsedArgs][errorOccured] Expected true.");
+        }
+
+        [TestMethod]
+        [TestCategory("ParsedArgs")]
+        public void containsKeyTest()
+        {
+            ParsedArgs args = new ParsedArgs();
+            Assert.IsFalse(args.containsKey("test"), "[ParsedArgs][containsKey] Expected false.");
+            args.add("test", "test");
+            Assert.IsTrue(args.containsKey("test"), "[ParsedArgs][containsKey] Expected true.");
+        }
     }
 }
