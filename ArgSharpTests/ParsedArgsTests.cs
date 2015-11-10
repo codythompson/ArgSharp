aa198d7f8e131b65d8afe9db77a251cf0265e3aa
codyThompson <cody.thompson22@gmail.com>
2015-11-09 21:42:29 -0800

    Created tests for getValue and related convenience methods.

--- ArgSharpTests/ParsedArgsTests.cs
+++ ArgSharpTests/ParsedArgsTests.cs
@@ -46,5 +46,58 @@ namespace ArgSharpTests
             args.add("test", "test");
             Assert.IsTrue(args.containsKey("test"), "[ParsedArgs][containsKey] Expected true.");
         }
+
+        [TestMethod]
+        [TestCategory("ParsedArgs")]
+        [ExpectedException(typeof(ParsedArgsKeyNotFoundException), "[ParsedArgs][getValue] Expected a ParsedArgsKeyNotFoundException to be thrown")]
+        public void getValueTestKeyNotFound()
+        {
+            ParsedArgs args = new ParsedArgs();
+            args.add("not this", "blah blah");
+            args.getValue<string>("yes this please");
+        }
+
+        [TestMethod]
+        [TestCategory("ParsedArgs")]
+        [ExpectedException(typeof(ParsedArgsWrongTypeException), "[ParsedArgs][getValue] Expected a ParsedArgsWrongTypeException to be thrown")]
+        public void getValueTestWrongType()
+        {
+            ParsedArgs args = new ParsedArgs();
+            args.add("a key", "whaaat, this isn't an int");
+            args.getValue<int>("a key");
+        }
+
+        [TestMethod]
+        [TestCategory("ParsedArgs")]
+        public void getStringTest()
+        {
+            ParsedArgs args = new ParsedArgs();
+            args.add("blah", "blah blah");
+            args.add("mmk", "");
+            Assert.AreEqual("blah blah", args.getString("blah"), "[ParsedArgs][getString] Encountered unexpected return value.");
+            Assert.AreEqual("", args.getString("mmk"), "[ParsedArgs][getString] Encountered unexpected return value.");
+        }
+
+        [TestMethod]
+        [TestCategory("ParsedArgs")]
+        public void geIntTest()
+        {
+            ParsedArgs args = new ParsedArgs();
+            args.add("blah", 2123123);
+            args.add("mmk", 0);
+            Assert.AreEqual(2123123, args.getInt("blah"), "[ParsedArgs][getInt] Encountered unexpected return value.");
+            Assert.AreEqual(0, args.getInt("mmk"), "[ParsedArgs][getInt] Encountered unexpected return value.");
+        }
+
+        [TestMethod]
+        [TestCategory("ParsedArgs")]
+        public void getDoubleTest()
+        {
+            ParsedArgs args = new ParsedArgs();
+            args.add("blah", 21.23123);
+            args.add("mmk", 0.0);
+            Assert.AreEqual(21.23123, args.getDouble("blah"), "[ParsedArgs][getDouble] Encountered unexpected return value.");
+            Assert.AreEqual(0.0, args.getDouble("mmk"), "[ParsedArgs][getDouble] Encountered unexpected return value.");
+        }
     }
 }
