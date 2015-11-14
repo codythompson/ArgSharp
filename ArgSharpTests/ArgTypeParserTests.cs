1955b1e11101f8fe13ec0bfad6940b4a40b8cc93
codyThompson <cody.thompson22@gmail.com>
2015-11-13 19:26:56 -0800

    Started creating a unit test for stringParser and realized that there are some design issues with the parsers. Am committing an empty test class to be completed after the issues with the parsers are fixed.

---
+++ ArgSharpTests/ArgTypeParserTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using ArgSharp;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ArgSharpTests
+{
+    [TestClass]
+    public class ArgTypeParserTests
+    {
+        //[TestMethod]
+        //[TestCategory("ArgTypeParserTests")]
+        //public void stringParserTest()
+        //{
+        //    stringParser parser = new stringParser();
+        //    string parseResult;
+            
+        //}
+    }
+}
